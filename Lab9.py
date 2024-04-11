47378b8ede6f9202d9ffbe6fe846090ec6597abe
asoto732 <andresd0711@gmail.com>
2024-04-11 11:48:30 -0400

    Lab 9
    ADDED THE DECODE FUNCTION

--- Lab9.py
+++ Lab9.py
@@ -8,6 +8,18 @@ def encode(password):
     return encoded_password
 
 
+def decode(encoded_password):
+    encoded_str = str(encoded_password)
+    decoded_password = " "
+    for char in encoded_str:
+        digit = int(char)
+        digit = (digit - 3 + 10) % 10
+        decoded_password += str(digit)
+    return decoded_password
+
+
+
+
 def main():
     while True:
         print("\nMenu:")
@@ -25,7 +37,7 @@ def main():
             encode(password)
             print("Your password has been encoded and stored!")
         elif choice == '2':
-            print(f"The encoded password is {encode(password)}, and the original password is {decode(password)}")
+            print(f"The encoded password is {encode(password)}, and the original password is {decode(encode(password))}")
         elif choice == '3':
             break
         else:
